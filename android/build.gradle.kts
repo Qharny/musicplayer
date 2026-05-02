c35f60e30e38f97e1be4f8523ebbd63d5eebbc7c
Qharny <kabuteymanasseh5@gmail.com>
2026-05-02 21:19:03 +0000

    fix: add dynamic namespace injector and use stable AGP/Kotlin versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,27 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val project = this
+    if (project.state.executed) {
+        configureAndroidNamespace(project)
+    } else {
+        project.afterEvaluate {
+            configureAndroidNamespace(project)
+        }
+    }
+}
+
+fun configureAndroidNamespace(project: Project) {
+    if (project.extensions.findByName("android") != null) {
+        @Suppress("UNCHECKED_CAST")
+        val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+        if (android.namespace == null) {
+            android.namespace = "com.example.musicplayer.${project.name.replace(":", ".")}"
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
